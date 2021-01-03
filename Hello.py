fad35673ba018e38d62ed47c1f85fe72ff36fd8b
mrfrozenpeak <40189138+mrfrozen97@users.noreply.github.com>
2021-01-03 23:51:47 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,2 +1,16 @@
-print("Hello to myself. I don't care about world.")
+#This code tells us if a noob coder named Sam Miller has small or big pp
 #i am noob so don't expect great codes
+import random
+
+#Sam_pp_max_size. Ofc in nanometer
+Max_pp_size = 10 #nm
+
+Sam_miller_pp = random.random()*Max_pp_size
+
+Not_ashamed_of_me_size = 6
+
+
+if Sam_miller_pp > Not_ashamed_of_me_size:
+      print ("Hello! I am Sam\nAnd I am proud of my pp.")
+else:
+      print ("I am small pp person.\n 1subscribe = +0.001nm") 
