c2237077fadb25191d9d0922af81286869b8f548
mrfrozenpeak <40189138+mrfrozen97@users.noreply.github.com>
2021-01-04 11:14:38 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,16 +1,38 @@
 #This code tells us if a noob coder named Sam Miller has small or big pp
 #i am noob so don't expect great codes
 import random
+import time
 
 #Sam_pp_max_size. Ofc in nanometer
 Max_pp_size = 10 #nm
+#I am just joking max size is not that much
 
 Sam_miller_pp = random.random()*Max_pp_size
 
-Not_ashamed_of_me_size = 6
+Not_ashamed_of_me_size = 5.5
 
 
-if Sam_miller_pp > Not_ashamed_of_me_size:
-      print ("Hello! I am Sam\nAnd I am proud of my pp.")
+print("You are fello programmer Help Sam get a date \U0001f600,\nChoose from below\n1)Lana\n2)Pokemaine\n3)Mia")
+pp_reduired = [5, 5.5, 6.5]
+date_list = ['Lana', 'Pokemaie', 'Mia']
+choice_for_date = int(input())
+
+if Sam_miller_pp > pp_reduired[choice_for_date-1] and Not_ashamed_of_me_size:
+      time.sleep(3)
+      print ("Hello! I am Sam\nAnd I am proud of my pp.\U0001F917")
+      time.sleep(3)
+      print(date_list[choice_for_date-1] + "- WOW!!! cool..... So big.......\U0001F617")
+      time.sleep(3)
+      print("Sam- Ya!! Thanks... I am a pro-grammer \U0001F619")
+      time.sleep(3)
+      print(date_list[choice_for_date-1] + "- What!!! A programmer .... Just F*#k Off!! Noob..... \U0001F620")
+      time.sleep(3)
+      print("Sam- OH!!! No I am still virgin\U0001F62D\n")
+
 else:
-      print ("I am small pp person.\n 1subscribe = +0.001nm") 
+      print ("Sam- Oh No!!!!! I am small pp person. \U0001F622")
+      time.sleep(3)
+      print("Guys 1 subscribe = +0.001nm\n")
+
+time.sleep(3)
+print("\nSupport Sam to get a life....... R.I.P \U0001F61E")
