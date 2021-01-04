c15b349ddf558ca7886b385a48f133a331ea11bb
mrfrozenpeak <40189138+mrfrozen97@users.noreply.github.com>
2021-01-04 11:17:52 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -5,17 +5,20 @@ import time
 
 #Sam_pp_max_size. Ofc in nanometer
 Max_pp_size = 10 #nm
-#I am just joking max size is not that much
+#I am just joking max size is just imagination
 
-Sam_miller_pp = random.random()*Max_pp_size
+Sam_miller_pp = random.random()*Max_pp_size                  #check sam's pp size
 
-Not_ashamed_of_me_size = 5.5
+Not_ashamed_of_me_size = 5.5                                 #threshold for sam to be called human adult. Otherwise ........
 
 
-print("You are fello programmer Help Sam get a date \U0001f600,\nChoose from below\n1)Lana\n2)Pokemaine\n3)Mia")
+print("You are fello programmer Help Sam get a date \U0001f600,\nChoose from below\n1)Lana\n2)Pokemaine\n3)Mia")              #Help sam.......
 pp_reduired = [5, 5.5, 6.5]
 date_list = ['Lana', 'Pokemaie', 'Mia']
-choice_for_date = int(input())
+choice_for_date = int(input())                                                     
+
+
+#Check if sam is good enough......................................
 
 if Sam_miller_pp > pp_reduired[choice_for_date-1] and Not_ashamed_of_me_size:
       time.sleep(3)
@@ -34,5 +37,7 @@ else:
       time.sleep(3)
       print("Guys 1 subscribe = +0.001nm\n")
 
+      
+#Unfortunately he is a programmer......... So don;t expect good things
 time.sleep(3)
 print("\nSupport Sam to get a life....... R.I.P \U0001F61E")
